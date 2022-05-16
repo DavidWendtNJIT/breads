a48172b8a0975ef5baa720cb2101bd079e64fd38
DavidWendtNJIT <djw5383@gmail.com>
2022-05-16 18:34:03 -0400

    forgot to commit this entire time

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require("express");
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 
 // CONFIGURATION
@@ -16,20 +17,35 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
+
+
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads')
+  res.redirect('/breads')
 })
 
-// Breads
+
+// breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// bakers 
+const bakersController = require('./controllers/bakers_controller.js')
+app.use('/bakers', bakersController)
+
 // 404 Page
 app.get('*', (req, res) => {
-  res.render('404')
+  res.send('404')
 })
+
 
 
 
